0e71b5d63fa3bfef9313ce6b5228fd86c9f59332
RashiRamesh <rashiramesh262@gmail.com>
2026-05-27 10:07:49 +0530

    Add Demo class to compare string references

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo{
+  public static void main(String[] args){
+    String s1="JAVA";
+    String s2="Java";
+    if(s1==s2){
+      System.out.println("Ref are equal");
+    }
+    else{
+      System.out.println("Ref are not equal");
+    }
+  }
+}
